c24a95f79096a0b589c875ed2bc97e4625223aab
ThatDott <justinsanveloso@gmail.com>
2026-09-06 06:30:40 +0800

    feat: init shadcn

--- apps/web-app/app/layout.tsx
+++ apps/web-app/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Roboto, Roboto_Slab } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const robotoSlabHeading = Roboto_Slab({subsets:['latin'],variable:'--font-heading'});
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "PaddlePit",
@@ -10,7 +16,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", roboto.variable, robotoSlabHeading.variable)}
     >
       <body className="min-h-full flex flex-col justify-center">{children}</body>
     </html>
