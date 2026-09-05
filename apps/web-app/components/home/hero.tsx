4545d488ae76417d83312a1b0ae12f20242133b7
ThatDott <justinsanveloso@gmail.com>
2026-09-06 07:44:28 +0800

    refactor: applied lsp suggestions

--- apps/web-app/components/home/hero.tsx
+++ apps/web-app/components/home/hero.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Hero() {
-  return (
-    <div className="flex flex-col h-[100vh] justify-center items-center gap-[32px]">
-      <div className="flex flex-row h-[fit] gap-[16px]">
-	<div className="flex aspect-square h-full text-white bg-primary justify-center items-center text-2xl rounded-2xl">Logo</div>
-	<h1 className="text-center font-bold text-[64px] text-primary">PaddlePit</h1>
-      </div>
-      <p className="text-center text-muted-foreground">Book your court in seconds. Real-time availability, instant confirmation.</p>
-      <Button className="w-[187px] h-[66px] text-xl text-white">Book Now</Button>
-    </div>
-  );
+	return (
+		<div className="flex flex-col h-screen justify-center items-center gap-[32]">
+			<div className="flex flex-row h-[fit] gap-[16]">
+				<div className="flex aspect-square h-full text-white bg-primary justify-center items-center text-2xl rounded-2xl">Logo</div>
+				<h1 className="text-center font-bold text-[64px] text-primary">PaddlePit</h1>
+			</div>
+			<p className="text-center text-muted-foreground">Book your court in seconds. Real-time availability, instant confirmation.</p>
+			<Button className="w-[187] h-[66] text-xl text-white">Book Now</Button>
+		</div>
+	);
 }
