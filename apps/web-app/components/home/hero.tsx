6ef172863a10d9200578e4b34b6c32175d37d4a8
ThatDott <justinsanveloso@gmail.com>
2026-09-08 05:59:07 +0800

    feat: hi-fi hero section

--- apps/web-app/components/home/hero.tsx
+++ apps/web-app/components/home/hero.tsx
@@ -2,13 +2,19 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
 	return (
-		<div className="flex flex-col h-screen justify-center items-center gap-[32]">
-			<div className="flex flex-row h-[fit] gap-[16]">
-				<div className="flex aspect-square h-full text-white bg-primary justify-center items-center text-2xl rounded-2xl">Logo</div>
-				<h1 className="text-center font-bold text-[64px] text-primary">PaddlePit</h1>
+		<div className="flex flex-col h-screen justify-center items-center gap-[32] bg-emerald-950">
+			<div className="flex flex-row items-center gap-[8] bg-gold/20 px-5 py-2 border border-gold rounded-3xl">
+				<span className="w-1.5 h-1.5 rounded-full bg-gold inline-block"></span>
+				<span className="text-gold text-xs font-semibold">DAVAO CITY, PHILIPPINES</span>
 			</div>
-			<p className="text-center text-muted-foreground">Book your court in seconds. Real-time availability, instant confirmation.</p>
-			<Button className="w-[187] h-[66] text-xl text-white">Book Now</Button>
+			<div className="flex flex-row h-[fit] gap-[16]">
+				<h1 className="text-center font-semibold text-[64px] font-[Lora] text-cream">Play. Reserve. <br /><span className="italic">Repeat.</span></h1>
+			</div>
+			<p className="text-center text-muted-foreground text-xl">Book your court in seconds. Real-time <br /> availability, instant confirmation.</p>
+			<div className="flex flex-row gap-[16]">
+				<Button variant="gold" size="xl" className="text-lg">Book a Court</Button>
+				<Button variant="cream" size="xl" className="text-lg">Check Availability</Button>
+			</div>
 		</div>
 	);
 }
